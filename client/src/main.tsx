f85a788241eaca8ff9a916fe615b40d586ce1e2d
Brandon Williams <brandon.williams79@gmail.com>
2026-07-05 07:19:04 -0500

    client: add react-query/iro/fontsource deps, self-host Plus Jakarta Sans, wire QueryClientProvider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { QueryClientProvider } from '@tanstack/react-query'
+import '@fontsource/plus-jakarta-sans/400.css'
+import '@fontsource/plus-jakarta-sans/500.css'
+import '@fontsource/plus-jakarta-sans/600.css'
+import '@fontsource/plus-jakarta-sans/700.css'
 import './index.css'
 import App from './App.tsx'
+import { createQueryClient } from './api/queryClient'
+
+const queryClient = createQueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
 )
